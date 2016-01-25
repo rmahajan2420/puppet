8d70e8266b34e8c3bc0087ca2ba84cf3132fa985
root <root@ip-10-0-0-10.ap-southeast-2.compute.internal>
2016-01-25 02:50:13 +0000

    removing

--- modules/gradle/files/gradle.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-gradle_version=2.10
-wget -c http://services.gradle.org/distributions/gradle-${gradle_version}-all.zip
-unzip gradle-${gradle_version}-all.zip -d /opt
-ln -s /opt/gradle-${gradle_version} /opt/gradle
-printf "export GRADLE_HOME=/opt/gradle\nexport PATH=\$PATH:\$GRADLE_HOME/bin\n" > /etc/profile.d/gradle.sh
-. /etc/profile.d/gradle.sh
